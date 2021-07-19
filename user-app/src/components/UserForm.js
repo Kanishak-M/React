f89e90e86aa60b12ab37e3a524f867c41f57b4a9
Kanishak-M <kanishak.m@timesinternet.in>
2021-07-19 16:08:48 +0530

    added date field

--- user-app/src/components/UserForm.js
+++ user-app/src/components/UserForm.js
@@ -1,7 +1,7 @@
 import React , {useState} from 'react'
-
+import axios from "axios"
 export default function UserForm() {
-    const [userName ,setUserName] = useState({name :"kanishak" ,age : 22})
+    const [userName ,setUserName] = useState({name :"kanishak" ,age : 22 ,todayDate : Date() })
     
     const userEventHandler = (event) => {
         console.log(event)
@@ -9,16 +9,27 @@ export default function UserForm() {
     }
     const userAgeEventHandler = (event) => {
         console.log(event)
-        setUserName({...userName , age : event.target.value})
+        setUserName({...userName , age : event.target.value, [event.target.name] : event.target.value})
+    }
+    const userDateChangeHAndler = (e) => {
+        setUserName({...userName , todayDate : e.target.value} )
+        console.log(userName.todayDate)
     }
     const save = (e) => {
-        console.log(userName)
+        try {
+            const promise = axios.post("http://localhost:4200/users",userName)
+            promise.then((response) =>{console.log(response)})
+        }catch(err){
+            console.log(err)
+        }
+     
     }
     return (
         <div>
-            {userName.name} <br/>
+            {userName.name}  and <br/>
             <input value={userName.name}  onChange ={userEventHandler}/> <br/>
             <input value={userName.age} onChange={userAgeEventHandler}/> <br/>
+            <input type="date"  onChange={userDateChangeHAndler} />
             <button onClick={save}>Save</button>
         </div>
     )
